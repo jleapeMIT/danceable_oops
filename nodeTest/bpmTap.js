0002441d8629621ed91a31a17516211b28aa6de7
jleapeMIT <jleape@mit.edu>
2018-05-04 20:32:32 -0400

    add gitignore

--- nodeTest/bpmTap.js
+++
@@ -1,7 +0,0 @@
-const BPM = require('bpm');
-const b = new BPM();
-
-b.tap();
-setTimeout(function() {
-  console.log(b.tap());
-}, 1000);
--- nodeTest/getLyrics.js
+++
@@ -1,8 +0,0 @@
-var lyr = require('lyrics-fetcher');
-
-const getLyrics = function(artist, title){
-    lyr.fetch(artist, title, function (err, lyrics) {
-        console.log(err || lyrics);
-        return err || lyrics;
-    });
-}
